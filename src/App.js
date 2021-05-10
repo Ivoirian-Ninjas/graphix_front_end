3dd1a86068bee644015355d62d89a0c2e9826cce
armel50 <armelfli50@gmail.com>
2021-05-10 16:19:36 -0500

    added facebook chatbot

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from './views/about'
 import Contact from './views/contact'
 import Service from './views/service'
 
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 import Navbar from './components/navbar'
 import Footer from './components/footer'
 
@@ -35,6 +37,13 @@ function App() {
         <Home />
       </Route>
     </Switch>
+
+    {/* The messenger chat will not be available on the local machine, Facebook needs to see an https signature. */}
+    {/* No design or css is required for this chat bot */}
+        <MessengerCustomerChat
+        pageId="108891984701971"
+        appId="294978968832597"
+      />
 
     <Footer />
   </Router>
