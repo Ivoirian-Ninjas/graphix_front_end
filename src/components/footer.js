f3c67fee8fb12af874f6910acbf563df89ea6d05
armel50 <armelfli50@gmail.com>
2021-05-13 22:54:06 -0500

    fixed text in the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import '../framework/fontawesome-free-5.11.2-web/css/all.css'
 export default function Footer() {
     return (
         <div className='divFooter'>
-            <h1 className='copyrightGraphix'> Copyright 2021 Graohix LLC</h1>
+            <h1 className='copyrightGraphix'> Copyright 2021 Graphix LLC</h1>
             <div className='divLogoFooter'>
                 <a href="/"><img src={logoFooter} alt='logo footer' className='imgFooter' /></a>
             </div>
